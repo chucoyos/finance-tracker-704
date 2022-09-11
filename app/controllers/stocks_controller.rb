4a79562db45cabad8fda8fca6ed2a400c7ccde29
chucoyos <facturasmanzanillo@hotmail.com>
2022-09-11 15:06:31 -0500

    add flash messages end validation for empty or invalid form requests

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -5,9 +5,15 @@ class StocksController < ApplicationController
   def search_stock
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
+      if @stock
       render 'stocks/my_portfolio'
+      else
+        flash[:alert] = "Please enter a valid symbol to search"
+        redirect_to my_portfolio_path
+      end
     else
-      render 'stocks/my_portfolio'
+      flash[:alert] = "Please enter a symbol to search"
+      redirect_to my_portfolio_path
     end
   end
 end
