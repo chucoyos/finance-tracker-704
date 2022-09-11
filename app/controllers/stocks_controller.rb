58cb5731295b2051bedc7ed0dccc3fa34c45fe07
chucoyos <facturasmanzanillo@hotmail.com>
2022-09-11 05:47:34 -0500

    set stock search functionality

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,4 +1,13 @@
 class StocksController < ApplicationController
   def my_portfolio
   end
+
+  def search_stock
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      render 'stocks/my_portfolio'
+    else
+      render 'stocks/my_portfolio'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "welcome#index"
   get "my_portfolio", to: "stocks#my_portfolio"
+  get "search_stock", to: "stocks#search_stock"
 end
