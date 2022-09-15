baa567c10e6b74bb32876a81d587335e568c5bcc
chucoyos <facturasmanzanillo@hotmail.com>
2022-09-14 22:17:51 -0500

    create User-Stocks relationship

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,7 +4,7 @@ class StocksController < ApplicationController
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
       if @stock
-      render 'users/my_portfolio'
+        render 'users/my_portfolio'
       else
         flash[:alert] = "Please enter a valid symbol to search"
         redirect_to my_portfolio_path
